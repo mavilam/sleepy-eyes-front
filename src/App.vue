528b40fdc958e8fdb64d9f161bdad1a2d04432cf
Miguel Ávila Monroy <mavilam@evobanco.com>
2022-05-25 14:22:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col h-screen">  
+    <Content class="flex-grow"/>
+    <Footer/>
+  </div>
+</template>
+
+<script setup>
+import Header from '././components/Header.vue'
+import Content from '././components/Content.vue'
+import Footer from '././components/Footer.vue'
+</script>
+
+<style>
+#app {
+  font-family: "Lato", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
